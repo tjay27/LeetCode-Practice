6539f1f9de72f1b9311a18e76ea13083636f8a76
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-03-03 14:32:27 +0530

    Time: 4 ms (49.49%), Space: 7.4 MB (55.42%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        
+        if(nums.size()<3)
+            return 0;
+        int possible=0;
+        int i=0;
+        
+        while(i<nums.size()-2){
+            int d=nums[i+1]-nums[i];
+            int j=i+1;
+            int len=2;
+            while(j<nums.size()-1){
+                if(nums[j+1]-nums[j]==d){
+                    len++;
+                    j++;
+                    i++;
+                }
+                else{
+                    i++;
+                    break;
+                }
+            }
+            if(len>=3){
+                int k=3;
+                while(k<=len){
+                    possible+= len-k+1;
+                    k++;
+                }
+            }
+        
+        }
+        return possible;
+    }
+};
