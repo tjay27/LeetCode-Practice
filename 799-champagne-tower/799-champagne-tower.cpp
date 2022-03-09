8d4d382932492e38119048790b438f0b097333cf
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-03-10 05:02:00 +0530

    Time: 7 ms (63.13%), Space: 6 MB (85.51%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,40 +1,21 @@
-/*class Solution {
+class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        double filledGlasses=(query_row+1)*(query_row+2)/2;
-        if(poured<filledGlasses){
-            double q=sqrt((poured-3)/4) -1.5;
-            int qt=q;
-            if(q-qt!=0){
-                if(query_glass==0||query_glass=query_row-1){
-                    
-                }
-            }
-        }
-        return 1;
-    }
-};
-*/
-
-
-class Solution {
-    double dp[101][101];
-public:
-    double champagneTower(int p, int qr, int qg) {
-        memset(dp,0,sizeof dp);
-        dp[0][0] = p;
-        for (int i = 0; i < 100; i++) 
-            for (int j = 0; j <= i; j++) {
-                if (dp[i][j] >= 1) {
-                    double x=(dp[i][j] - 1) / 2.0;
-                    dp[i][j] = 1;
-                    
-                    dp[i + 1][j] += x;
-                    dp[i + 1][j + 1] +=x;
-                    
-                }
-            }
+        //bottom up
+        double dp[101][101]={0};
+        dp[0][0]=poured;
         
-        return dp[qr][qg];
+        for(int i=0;i<=query_row;i++){
+            for(int j=0;j<=i;j++){
+                if(dp[i][j]>1){
+                    dp[i+1][j]+=(dp[i][j]-1)/2;
+                    dp[i+1][j+1]+=(dp[i][j]-1)/2;
+                    dp[i][j]=1;
+                }              
+            } 
+        }
+        
+        return dp[query_row][query_glass];        
+        
     }
 };
