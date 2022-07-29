16f05e1697856005769b9e36749c62d3de6eb0a8
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-07-29 20:21:45 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (40.38%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,26 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    void inorder(TreeNode*root,vector<int> &ans){
+    //recursive
+    void inorder(TreeNode* root, vector<int> &ans){
         if(root==NULL)
             return ;
         inorder(root->left,ans);
         ans.push_back(root->val);
         inorder(root->right,ans);
     }
+    //Inorder Iterative is Recursion with a Stack space of your own
+    void inorderIt(TreeNode* root, vector<int> &ans){
+        stack<TreeNode*> st;
+       
+        while(root!=NULL||!st.empty()){
+            while(root!=NULL){
+                st.push(root);
+                root=root->left;
+            }
+            ans.push_back(st.top()->val);
+            root=st.top()->right;
+            st.pop();
+        }
+        
+        return ;
+}   
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root,ans);
+        inorderIt(root,ans);
         return ans;
     }
 };
+
+
+
+
