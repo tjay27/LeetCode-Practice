19b0d8ede366b3cfdd465ab3e62c43cbfbe1f511
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-07-30 12:24:27 +0530

    Time: 4 ms (99.21%), Space: 21.7 MB (30.67%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,4 +1,7 @@
 class Solution {
+    //Not a good time complexity
+    //O(n)
+    //O(n)
     TreeNode* max(TreeNode* root){
         if(root==NULL)
             return root;
@@ -14,8 +17,19 @@ class Solution {
             root=root->left;
         return root;
     }
+    bool isValid(TreeNode* root, long min, long max){
+        if(root==NULL)
+            return true;
+        if(root->val>min&&root->val<max){
+            bool leftAns=isValid(root->left, min, root->val);
+            bool rightAns=isValid(root->right, root->val, max);
+            return leftAns&rightAns;
+        }
+        return false;
+        
+    }
 public:
-    bool isValidBST(TreeNode* root) {
+    /*bool isValidBST(TreeNode* root) {
         if(root==NULL)
             return true;
         if(!root->left&&!root->right)
@@ -31,5 +45,9 @@ public:
                 return maxLeft->val<root->val;
         }
         return false;
+    }*/
+    bool isValidBST(TreeNode* root) {
+        //cout<<LONG_MIN;
+        return isValid(root, LONG_MIN, LONG_MAX);   
     }
 };
