33b843124d1bb2baae0936c0c67cb27b4ac471d2
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-07-31 13:27:00 +0530

    Time: 181 ms (100.00%), Space: 56.6 MB (100.00%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
-        int k = 1;
-        int ans = 0;
-        int i = 0;
-        while(i < grades.size()) {
-            if (k > grades.size() - i){
-                return ans;
-            }
-            i = i + k;
-            ans++;
-            k++;
+        //3 5 6 7 10 12
+        //total grps will be less than n
+        //FINd that i jiske lie i(i+1)/2 is less than n
+        int i=grades.size();
+        int n=grades.size();
+        while(i>0&&n<long(i)*long(i+1)/2){
+            i--;
         }
-        return ans;
+        return i;
     }
 };
