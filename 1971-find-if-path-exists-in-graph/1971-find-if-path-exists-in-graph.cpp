9cf470134108e5e3120e6e6624ac106c7892737a
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-08-02 21:11:01 +0530

    Time: 1272 ms (26.85%), Space: 174.8 MB (54.94%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,62 +1,56 @@
-// class Solution {
-// #include<vector>
-// bool pathExists(int start,vector<vector<int>> adjList, vector<int>&visited,int dest){
-//     if(start==dest)
-//         return true;
-//     visited[start]=1;
-//     for(int j=0;j<adjList[start].size();j++){
-//         if(visited[adjList[start][j]]!=1){
-//             bool ans=pathExists(adjList[start][j],adjList,visited,dest);
-//             if(ans)
-//                 return ans;
-//         }
-//     }
-
-//     return false;
-// }
-
-// public:
-//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-//         //construct adjList
-//         vector<int> visited(n,0);
-//         vector<vector<int>> adjList(n);
-//         for(int i=0;i<edges.size();i++){
-//             int a=edges[i][0];
-//             int b=edges[i][1];
-//             adjList[a].push_back(b);
-//             adjList[b].push_back(a);
-//         }
-        
-        
-//        return pathExists(source, adjList, visited, destination);
-//        return true; 
-//     }
-// };
 class Solution {
+#include<vector>
+#include<stack>
+bool pathExists(int start,vector<vector<int>> adjList, vector<int>&visited,int dest){
+    if(start==dest)
+        return true;
+    visited[start]=1;
+    for(int j=0;j<adjList[start].size();j++){
+        if(visited[adjList[start][j]]!=1){
+            bool ans=pathExists(adjList[start][j],adjList,visited,dest);
+            if(ans)
+                return ans;
+        }
+    }
+
+    return false;
+}
+bool dfs_iter(int start,vector<vector<int>> adjList, vector<int>&visited,int end){
+    if(start==end)
+        return true;
+    visited[start]=true;
+    stack<int> st;
+    st.push(start);
+    while(!st.empty()){
+        int front=st.top();
+        st.pop();
+        for(auto i: adjList[front]){
+            if(visited[i]==1)
+                continue;
+            st.push(i);
+            visited[i]=1;
+            if(i==end)
+                return true;
+        }
+    }
+    
+    return false;
+}
+
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        unordered_map<int,vector<int>> graph; 
-        for(auto e : edges) {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        //construct adjList
+        vector<int> visited(n,0);
+        vector<vector<int>> adjList(n);
+        for(int i=0;i<edges.size();i++){
+            int a=edges[i][0];
+            int b=edges[i][1];
+            adjList[a].push_back(b);
+            adjList[b].push_back(a);
         }
-        vector<bool> visited(n,0);        
-        stack<int> st;
-        st.push(start);
-        visited[start] = 1; 
         
-        while(!st.empty()){ 
-            auto top = st.top();
-            if(top == end)
-                return 1;
-            st.pop();
-            for(auto node : graph[top]){
-                if(!visited[node]){
-                    visited[node] = 1;
-                    st.push(node); 
-                }
-            }
-        }
-        return false;
+        
+       return dfs_iter(source, adjList, visited, destination);
+
     }
 };
