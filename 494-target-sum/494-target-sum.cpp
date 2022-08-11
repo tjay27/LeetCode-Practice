68a0f3de11f186820a1edf01ca1bb5a2c5899447
Tejaswini Gurram <47633397+tjay27@users.noreply.github.com>
2022-08-11 19:34:17 +0530

    Time: 27 ms (55.60%), Space: 9.4 MB (54.07%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -10,23 +10,26 @@ public:
         if((s+target)%2!=0)
             return 0;
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+       // vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
         
         //initialisation
-        for(int i=0;i<=sum;i++){
-            dp[0][i]=0;
-        }
-        dp[0][0]=1;
         
+        vector<int> prev(sum+1);
+        vector<int> curr(sum+1);
+        for(int i=0;i<=sum;i++){
+            prev[i]=0;
+        }
+        prev[0]=1;
         for(int i=1;i<=n;i++){
             for(int j=0;j<=sum;j++){
                 if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
+                    curr[j]=prev[j-nums[i-1]]+prev[j];
                 }
                 else
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
             }
+            prev=curr;
         }
-        return dp[n][sum];
+        return curr[sum];
     }
 };
